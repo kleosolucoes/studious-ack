58a05b9b829da9b539297ea104bde3b10d468867
Leonardo Pereira Magalhães <falecomleonardopereira@gmail.com>
2018-09-29 16:46:41 -0300

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -16,5 +16,5 @@ php composer.phar require facebook/webdriver
 sudo wget https://github.com/mozilla/geckodriver/releases/download/v0.16.1/geckodriver-v0.16.1-linux64.tar.gz
 sudo sh -c 'tar -x geckodriver -zf geckodriver-v0.16.1-linux64.tar.gz -O > /usr/bin/geckodriver'
 sudo chmod +x /usr/bin/geckodriver
-rm geckodriver-v0.16.1-linux64.tar.gz
+sudo rm geckodriver-v0.16.1-linux64.tar.gz
 
